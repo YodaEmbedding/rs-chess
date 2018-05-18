68ee864e2dd8940c282be45cd6370a5b17dc399e
Mateen Ulhaq <mulhaq2005@gmail.com>
2018-05-18 00:35:31 -0700

    a-b

--- src/search.rs
+++ src/search.rs
@@ -1,3 +1,6 @@
+use std;
+use std::cmp;
+
 use game::Game;
 use moves::Move;
 use position::Position;
@@ -9,21 +12,31 @@ impl Game {
 
         self.position.get_moves(&self.move_generator).into_iter()
             .map(|m| (m, self.position.make_move(m)))
-            .map(|(m, p)| (m, self.negamax(&p, 3, -color)))
-            .max_by_key(|(m, e)| color * e)
+            .map(|(m, p)| (m, self.negamax(&p, 3, -color, std::i32::MIN + 1, std::i32::MAX)))
+            .max_by_key(|(m, v)| color * v)
             .unwrap()
     }
 
-    fn negamax(&self, position: &Position, depth: u32, color: i32) -> i32 {
-        // evaluate() is + if white is winning
+    fn negamax(&self, position: &Position, depth: u32, color: i32,
+        alpha: i32, beta: i32) -> i32 {
+
         if depth == 0 { return position.evaluate(); }
 
-        color *
-        position.get_moves(&self.move_generator).into_iter()
-            .map(|m| position.make_move(m))
-            .map(|p| color * self.negamax(&p, depth - 1, -color))
-            .max()
-            .unwrap()
+        let mut a = alpha;
+        let mut best = std::i32::MIN;
+        let positions = position.get_moves(&self.move_generator).into_iter()
+            .map(|m| position.make_move(m));
+
+        for p in positions {
+            let v = color * self.negamax(&p, depth - 1, -color, -beta, -a);
+            best = cmp::max(v, best);
+            a    = cmp::max(v, a);
+            if a >= beta { break; }
+        }
+
+        color * best
     }
+
+    // TODO transposition tables
 }
 
